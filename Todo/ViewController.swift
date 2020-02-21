6c52d38690b27131d0e4769e9e24c1671a8eb54f
nishigakai <tai.nsgk@fuji.waseda.jp>
2020-02-21 21:51:28 +0900

    Initial Commit

---
+++ Todo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todo
+//
+//  Created by Nishigaki Taichi on 2020/02/21.
+//  Copyright © 2020 Nishigaki Taichi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
